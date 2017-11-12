6216a351159cf62b3ca2551df5a1278d9e42d1f9
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2017-11-12 14:17:52 -0500

    Remove duplicate backfill_ignorant_backsim definitions

--- sample/high-state-forecasts.R
+++ sample/high-state-forecasts.R
@@ -165,26 +165,6 @@ get_observation_values = function(voxel.data, target_trajectory_preprocessor, ta
   return (observion.values)
 }
 
-backfill_ignorant_backsim = function(voxel.data, signal.name) {
-  old.dat = voxel.data[["epidata.df"]] %>>%
-    dplyr::filter(season != voxel.data[["season"]]) %>>%
-    dplyr::group_by(season) %>>%
-    ## todo remove hard reference to wili
-    dplyr::filter(!any(is.na(wili))) %>>%
-    dplyr::ungroup() %>>%
-    {split(.[[signal.name]], .[["Season"]])}
-  new.dat = voxel.data[["epidata.df"]] %>>%
-    dplyr::filter(season == voxel.data[["season"]]) %>>%
-    magrittr::extract2(signal.name)
-  new.dat.sim = match.new.dat.sim(new.dat)
-  voxel.Season = season_to_Season(
-    voxel.data[["season"]], voxel.data[["first.week.of.season"]])
-  ## concatenate new.dat.sim onto old.dat, setting the :
-  full.dat = c(old.dat,
-               setNames(list(new.dat.sim), voxel.Season))
-  return (full.dat)
-}
-
 signal.name = "wili" # xxx should use "ili" rather than "wili" (above as well)
 
 current.issue.sw =
