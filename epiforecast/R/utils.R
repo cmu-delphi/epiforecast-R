f899f8ae5d8dfeb9cdf961f73bf7ffefbdaa6f0b
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2019-02-28 12:39:11 -0500

    Add unite_arraylike function to utils

--- epiforecast/R/utils.R
+++ epiforecast/R/utils.R
@@ -19,6 +19,8 @@
 ## along with epiforecast.  If not, see <http://www.gnu.org/licenses/>.
 ## license_header end
 
+##' @import R.utils
+NULL
 
 ##' A \code{seq} variant that produces a 0-length vector when \code{!(from <=
 ##' to)}.
@@ -95,3 +97,87 @@ dat.to.matrix3 = function(dat, n) {
 ##'
 ##' @export
 dat.to.matrix = dat.to.matrix3
+
+##' Reshape arraylike into fewer but larger dimensions and/or permute (and optionally rename) dimensions; like tidyr::unite on arrays using R.utils::wrap.array
+##'
+##' Collapses specified array(like) object dimensions together to produce an
+##' array with fewer but larger dimensions and an equal number of entries;
+##' indices of each collapsed dimension refers to a Cartesian product over the
+##' indices of the dimensions it was made from. This operation should be similar
+##' or equivalent to melting the array, calling tidyr::unite, and casting back
+##' into an array.
+##'
+##' Collapsed/united dimensions are ordered after all untouched dimensions. To
+##' tweak the ordering or use this function to simply permute dimensions,
+##' individual dimnames names can be included as their own singleton "sets" in
+##' dnn.sets.
+##'
+##' @param arraylike the arraylike object to reshape; must have the dimnames
+##'     names referred to in \code{{dnn.sets}}
+##'
+##' @param dnn.sets a list of character vectors; each character vector specifies
+##'     some dimensions to unite in the array; ordering within each vector
+##'     determines the ordering of indices in the resulting collapsed dimension
+##'     (as in R.utils::wrap.array); ordering between the vectors determines the
+##'     ordering of the collapsed dimensions in the result; names of vector
+##'     entries (e.g., \code{names(dnn.sets[[1]])}) are ignored;
+##'     \code{names(dnn.sets)}, if specified and nonblank, override automatic
+##'     names of the collapsed dimensions
+##'
+##' @param sep length-1 character vector; string used to combine dimnames for
+##'     collapsed dimensions, as well as automatic dimnames names for collapsed
+##'     dimensions
+##'
+##' @examples
+##' arraylike = array(1:2^5, rep(2,5))
+##' dimnames(arraylike) <- list(A=c("a1","a2"),B=c("b1","b2"),C=c("c1","c2"),D=c("d1","d2"),E=c("e1","e2"))
+##' ## Collapsing A&B, C&D:
+##' dimnames(unite_arraylike(arraylike, list(c("A","B"), c("C","D"))))
+##' ## Adjusting `sep` changes resulting dimnames and dimnames names:
+##' dimnames(unite_arraylike(arraylike, list(c("A","B"), c("C","D")), sep="__"))
+##' ## Result dimnames names can be manually specified:
+##' names(dimnames(unite_arraylike(arraylike, list(c("A","B"), DVD=c("C","D")), sep="__")))
+##' ## Singleton sets can be used to permute dimensions, optionally renaming them (changing the dimnames names):
+##' ##  Place dimension A at end:
+##' names(dimnames(unite_arraylike(arraylike, list("A"))))
+##' ##  Permute all dims:
+##' names(dimnames(unite_arraylike(arraylike, list("E","D","C","B","A"))))
+##' ##  Place some dimensions at end and rename one:
+##' names(dimnames(unite_arraylike(arraylike, list("A","EEE"="E"))))
+##' ## Collapsing and permuting are actually the same operation and can be mixed:
+##' names(dimnames(unite_arraylike(arraylike, list(c("C","D"),"A","EEE"="E"))))
+##'
+##' @export
+unite_arraylike = function(arraylike, dnn.sets, sep=".") {
+    src.dnns = names(dimnames(arraylike))
+    if (is.null(src.dnns)) {
+        stop ('Arraylike must have named dimnames.')
+    }
+    dim.i.sets = dnn.sets %>>%
+        lapply(function(selected.src.dnns) match(selected.src.dnns, src.dnns))
+    all.selected.dim.is = Reduce(c, dim.i.sets)
+    all.selected.dnns = Reduce(c, dnn.sets)
+    if (any(is.na(all.selected.dim.is))) {
+        stop (paste0('Dimensions in dnn.sets were not found in arraylike: ',
+                     paste(all.selected.dnns[is.na(all.selected.dim.is)], collapse=", ")))
+    }
+    if (anyDuplicated(all.selected.dnns) != 0L) {
+        stop (paste0('Dimension(s) referred to more than once in dnn.sets.  Duplicates: ',
+                     paste(all.selected.dnns[duplicated(all.selected.dnns)], collapse=", ")))
+    }
+    untouched.dim.is = setdiff(seq_along(src.dnns), all.selected.dim.is)
+    untouched.dnns = src.dnns[untouched.dim.is]
+    if (any(untouched.dnns %in% all.selected.dnns)) {
+        stop (paste0('Dimensions in dnn.sets could not be uniquely identified.  Dimnames names duplicates referenced in dnn.sets: ',
+                     paste(untouched.dnns %>>% `[`(.%in%all.selected.dnns), collapse=", ")))
+    }
+    arraylike %>>%
+        R.utils::wrap.array(c(as.list(untouched.dim.is), dim.i.sets), sep=sep) %>>%
+        {
+            automatic.union.names = sapply(dnn.sets, paste0, collapse=sep)
+            names(dim.i.sets) <- namesp(dim.i.sets) %>>%
+                {.[.==""] <- automatic.union.names[.==""]; .}
+            names(dimnames(.)) <- c(src.dnns[untouched.dim.is], names(dim.i.sets))
+            .
+        }
+}
