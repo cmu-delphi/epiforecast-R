d90bd6730d66eac7cc405720edc5e465777cdedb
Logan C. Brooks <lcbrook+github@andrew.cmu.edu>
2024-07-16 02:04:19 -0700

    Bug disclaimer for historical script

--- sample/natreg-for-collaborative-ensemble.R
+++ sample/natreg-for-collaborative-ensemble.R
@@ -1,1 +1,3 @@
-See the can-use-for-2018/2019-natreg-collab-ensemble branch for scripts to generate component forecasts to send in to be incorporated into the FluSight-Network ensemble.
+## See the can-use-for-2018/2019-natreg-collab-ensemble branch for scripts to generate component forecasts to send in to be incorporated into the FluSight-Network ensemble.
+
+## But note that the models on that branch are now outdated; e.g., EB has a critical bug that has been fixed on main but not on that branch yet, and there may be other missing fixes as well.
